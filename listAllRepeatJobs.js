0aed4eeecda9c0278a3b0b28fb8e8aae26da5388
Paul Deng <dengpeng.cn@gmail.com>
2019-06-16 01:06:47 +1000

    count jobs in queue

--- listAllRepeatJobs.js
+++ listAllRepeatJobs.js
@@ -2,6 +2,9 @@ const Queue = require("bull");
 const cronDelay = new Queue("cronDelay");
 
 async function listAllRepeatJobs() {
+  const numJobs = await cronDelay.count();
+  console.log("Jobs in the queue: " + numJobs);
+
   const jobs = await cronDelay.getRepeatableJobs();
   console.log(JSON.stringify(jobs));
 }
