bcbfc33d6f5982d58513bb7c6bc08816a732fb31
Paul Deng <dengpeng.cn@gmail.com>
2019-06-16 01:41:55 +1000

    change remove job

--- removeJob.js
+++ removeJob.js
@@ -3,13 +3,13 @@ const cronDelay = new Queue("cronDelay");
 
 async function removeRepeatJobs() {
   const jobs = await cronDelay.getRepeatableJobs();
-  if (jobs.len > 0) {
-    console.log("Deleting first job");
+//  if (jobs.len > 0) {
+//    console.log("Deleting first job");
     const result = await cronDelay.removeRepeatableByKey(jobs[0].key);
     console.log(result);
-  } else {
-    console.log("No job found in the quue");
-  }
+//  } else {
+//    console.log("No job found in the queue");
+//  }
 }
 
 removeRepeatJobs().then(() => process.exit());
